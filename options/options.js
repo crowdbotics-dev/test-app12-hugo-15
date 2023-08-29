7ea13c92c2068c444808923a8b2ab05a0549a9df
Hugo Seabra <hugo.seabra@crowdbotics.com>
2023-08-29 13:22:13 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "test-app12-hugo-15",
   url: "https://test-app12-hugo-15.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
